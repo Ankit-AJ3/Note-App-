d07dfb7f4a1080642dd9779c28e63f2ea1e9f265
Ankit Jangid <ankitjangid8501@gmail.com>
2023-11-12 12:42:28 +0530

    first commit

---
+++ src/Components/Note.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MdDeleteForever} from 'react-icons/md';
+
+const Note = () => {
+  return (
+    <div className='note'>
+    <span>Hello this is first note..</span>
+      <div className='note-footer'>
+        <small>13/04/2021</small>
+        <MdDeleteForever className='delete-icon' size='1.3em' />
+      </div>
+    </div>
+  )
+}
+
+export default Note
---
+++ src/Components/NotesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Note from './Note'
+
+const NotesList = () => {
+  return (
+    <div>
+      <Note/>
+    </div>
+  )
+}
+
+export default NotesList
